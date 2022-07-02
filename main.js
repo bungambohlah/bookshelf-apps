76ce58c3b34320b538763033fe9cc732029e9131
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-07-02 17:14:43 +0700

    feat: :sparkles: add functionality to update book with sweetalert2 popup

--- main.js
+++ main.js
@@ -132,23 +132,33 @@ function setIncompleteHtml(bookIncomplete = []) {
             <h3 class="text-gray-900 text-xl leading-tight font-medium mb-4">$title</h3>
             <div class="flex space-x-4">
               <p class="w-1/2 text-gray-700 text-base">Oleh: $author</p>
-              <div class="w-1/2">
-                <button title="Selesai dibaca" type="button" onclick="readedBook($id)" 
-                  class="text-white bg-green-700 hover:bg-green-800 focus:ring-2 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                  <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-                  </svg>
-                </button>
-                <button title="Hapus buku" type="button" onclick="removeBook($id)"
-                  class="text-white bg-rose-700 hover:bg-rose-800 focus:ring-2 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">
-                  <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
-                    </path>
-                  </svg>
-                </button>
+              <div class="w-1/2 grid sm:grid-cols-2 md:grid-cols-3 sm:gap-2">
+                <div class="w-4/12">
+                  <button title="Selesai dibaca" type="button" onclick="readedBook($id)" 
+                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-2 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                    </svg>
+                  </button>
+                </div>
+                <div class="w-4/12">
+                  <button title="Ubah buku" type="button" onclick="showEditBook($id)"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                  </button>
+                </div>
+                <div class="w-4/12">
+                  <button title="Hapus buku" type="button" onclick="removeBook($id)"
+                    class="text-white bg-rose-700 hover:bg-rose-800 focus:ring-2 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">
+                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                      </path>
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
           </article>`;
@@ -184,25 +194,35 @@ function setCompleteHtml(bookComplete = []) {
           <h3 class="text-gray-900 text-xl leading-tight font-medium mb-4">$title</h3>
           <div class="flex space-x-4">
             <p class="w-1/2 text-gray-700 text-base">Oleh: $author</p>
-            <div class="w-1/2">
-              <button title="Belum Selesai dibaca" type="button" onclick="revertBook($id)"
-                class="text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-2 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
-                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zM4.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z">
-                  </path>
-                </svg>
-              </button>
-              <button title="Hapus buku" type="button" onclick="removeBook($id)"
-                class="text-white bg-rose-700 hover:bg-rose-800 focus:ring-2 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">
-                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
-                  </path>
-                </svg>
-              </button>
+            <div class="w-1/2 grid sm:grid-cols-2 md:grid-cols-3 sm:gap-2">
+              <div class="w-4/12">
+                <button title="Belum Selesai dibaca" type="button" onclick="revertBook($id)"
+                  class="text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-2 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+                  <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zM4.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z">
+                    </path>
+                  </svg>
+                </button>
+              </div>
+              <div class="w-4/12">
+                <button title="Ubah buku" type="button" onclick="showEditBook($id)"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                </button>
+              </div>
+              <div class="w-4/12">
+                <button title="Hapus buku" type="button" onclick="removeBook($id)"
+                  class="text-white bg-rose-700 hover:bg-rose-800 focus:ring-2 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">
+                  <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                    </path>
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
         </article>`;
@@ -372,6 +392,83 @@ function findBook(event) {
   getBooks(searchBookTitle);
 }
 searchBook.addEventListener("submit", findBook);
+
+// add a function to show popup when user click on edit book button
+function showEditBook(id) {
+  try {
+    validate(id, "id");
+
+    const books = parseStorage();
+    const indexBook = books.findIndex((x) => x.id === id);
+    const currentBook = books[indexBook];
+    const title = `${currentBook.inputBookTitle} (${currentBook.inputBookYear})`;
+
+    Swal.fire({
+      title: `Ubah Buku ${title}`,
+      html: `<input id="editInputBookTitle" name="editInputBookTitle" type="text" required placeholder="Judul Buku" class="block w-full px-3 py-2 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500" value="${currentBook.inputBookTitle}" />
+            <div class="flex space-x-4">
+              <input id="editInputBookAuthor" name="editInputBookAuthor" type="text" required placeholder="Penulis" class="mt-3 block w-6/12 px-3 py-2 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                invalid:border-pink-500 invalid:text-pink-600
+                focus:invalid:border-pink-500 focus:invalid:ring-pink-500" value="${currentBook.inputBookAuthor}" />
+              <input id="editInputBookYear" name="editInputBookYear" type="number" required placeholder="Tahun" class="mt-3 block w-6/12 px-3 py-2 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                invalid:border-pink-500 invalid:text-pink-600
+                focus:invalid:border-pink-500 focus:invalid:ring-pink-500" value="${currentBook.inputBookYear}" />
+            </div>`,
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Ubah",
+      focusConfirm: false,
+      preConfirm: () => {
+        return {
+          inputBookTitle: document.getElementById("editInputBookTitle").value,
+          inputBookAuthor: document.getElementById("editInputBookAuthor").value,
+          inputBookYear: document.getElementById("editInputBookYear").value,
+        };
+      },
+    }).then(({ value: formValues }) => {
+      if (formValues) {
+        updateBook(id, formValues);
+      }
+    });
+  } catch (error) {
+    return error.message;
+  }
+}
+
+// add a function to handle edit book
+function updateBook(id, data) {
+  try {
+    validate(id, "id");
+
+    let books = parseStorage();
+    const indexBook = books.findIndex((x) => x.id === id);
+    data.id = id;
+    data.inputBookIsComplete = books[indexBook].inputBookIsComplete;
+    books[indexBook] = data;
+
+    stringifyStorageArray(books);
+    getBooks();
+
+    const title = `${books[indexBook].inputBookTitle} (${books[indexBook].inputBookYear})`;
+    Swal.fire({
+      icon: "success",
+      title: `Buku ${title} telah berhasil diubah.`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    return error.message;
+  }
+}
 
 // call document / windows function
 docReady(function () {
